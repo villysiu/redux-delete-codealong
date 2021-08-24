7a9672edc4eed34eab24f4c8c43fd8a2f4a40bf5
villysiu <villysiu@gmail.com>
2021-08-24 14:44:42 -0700

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,11 +1,28 @@
+import uuid from "uuid"; // generate random ID, less orone to err in virtual dom
+
+
 export default function manageTodo(state = {
   todos: [],
 }, action) {
   console.log(action)
   switch (action.type) {
-    case 'ADD_TODO':
 
-      return { todos: state.todos.concat(action.payload.text) };
+    // Add uuid, now storing an array of object
+    case 'ADD_TODO':
+      const todo = {
+        id: uuid(),
+        text: action.payload.text,
+      };
+      return { todos: state.todos.concat(todo) };
+
+      
+      // Store an Array of string
+      // return { todos: state.todos.concat(action.payload.text) };
+
+    case "DELETE_TODO":
+      
+      return { todos: state.todos.filter((todo) => todo.id !== action.payload)}
+  
 
     default:
       return state;
